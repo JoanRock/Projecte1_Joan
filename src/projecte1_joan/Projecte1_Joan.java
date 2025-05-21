149ae913c7372fa660358325780b6335108ec815
JoanRock <joanlopezclotet@gmail.com>
2025-05-21 20:16:45 +0200

    Adeu afegit

--- src/projecte1_joan/Projecte1_Joan.java
+++ src/projecte1_joan/Projecte1_Joan.java
@@ -4,7 +4,7 @@ package projecte1_joan;
 public class Projecte1_Joan {
 
     public static void main(String[] args) {
-        System.out.println("Nos vamos pal keli");
+        System.out.println("Hola");
     }
 
 }
